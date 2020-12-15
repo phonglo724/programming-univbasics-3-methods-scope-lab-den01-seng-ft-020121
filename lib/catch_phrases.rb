a8504bbc03e4ff87eecc22fabe56bb53aa6dc43c
Phong Lo <phonglo724@gmail.com>
2020-12-15 17:58:46 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,6 +1,7 @@
 def mario
   phrase = "It's-a me, Mario!"
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  link = "It's Dangerous To Go Alone! Take This."
   puts phrase
 end
 
